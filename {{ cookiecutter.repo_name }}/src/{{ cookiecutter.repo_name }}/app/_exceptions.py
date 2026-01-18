9a5cf288e98dce46c5ebc4d2729eb879ef74e851
Alex Key <alexk3y@gmail.com>
2026-01-18 17:56:08 +0400

    Improve requirements flow, bump versions

---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.repo_name }}/app/_exceptions.py
@@ -0,0 +1,15 @@
+# TODO(axk): Add app-layer exceptions.
+
+"""Exception hierarchy.
+
+Defines the class hierarchy for application-layer exceptions:
+
+BaseException
+ ├── BaseExceptionGroup
+ ├── ...
+ └── Exception
+      ├── FooError
+      │    ├── BarError
+      │    ├── BazError
+      │    └── QuxError
+"""
